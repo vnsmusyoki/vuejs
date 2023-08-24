1499eb3f9d9095b3ca0675d99cd9e85acc123391
Gloria Mutungi <33417391+Gloriahmutungi@users.noreply.github.com>
2023-08-24 22:14:05 +0300

    wjdehbde

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,7 @@ const router = createRouter({
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      name: 'about', 
       component: () => import('../views/AboutView.vue')
     }
   ]
